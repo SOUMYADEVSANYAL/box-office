552e6fac65b13d720bd90ac7cd1a3ab2b51a7c3e
Soumyadev Sanyal <soumya.sp35@gmail.com>
2022-02-14 00:08:29 +0530

    Created input and search button and fetch results form TV Maze api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,44 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/function-component-definition */
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
-  return <MainPageLayout>This is Home Page</MainPageLayout>;
+  const [input, setInput] = useState('');
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onSearch = () => {
+    const apilink = `https://api.tvmaze.com/search/shows?q=${input}`;
+    // https://api.tvmaze.com/search/shows?q=John
+    fetch(apilink)
+      .then(r => r.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={() => {}}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
 };
 
 export default Home;
