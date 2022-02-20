648f59bef05dff1a5eefa81ee1c0841fd9ec5cbf
Soumyadev Sanyal <soumya.sp35@gmail.com>
2022-02-20 20:21:25 +0530

    Added loading and error messages to the show page.

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -7,14 +7,43 @@ const Show = () => {
   const { id } = useParams();
 
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   //   https://api.tvmaze.com/shows/1?embed[]=seasons&embed[]=cast
   useEffect(() => {
-    apiGet(`shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-      setShow(results);
-    });
+    let isMounted = true;
+    apiGet(`shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        setTimeout(() => {
+          if (isMounted) {
+            setShow(results);
+            setIsLoading(false);
+          }
+        }, 2000);
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
 
   console.log('show', show);
+
+  if (isLoading) {
+    return <div>Data is getting loaded.</div>;
+  }
+
+  if (error) {
+    return <div>Error occured: {error}</div>;
+  }
+
   return <div>This is the show page</div>;
 };
 
