27ea2bea0c01fe69f2b1970fbde3fb8a82ede398
Soumyadev Sanyal <soumya.sp35@gmail.com>
2022-02-22 21:14:17 +0530

    Defined theme for styled components and created a few styled components.

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,12 +1,13 @@
 /* eslint-disable react/function-component-definition */
 import React from 'react';
+import { TitleWrapper } from './Title.styled';
 
 const Title = ({ title, subTitle }) => {
   return (
-    <div>
+    <TitleWrapper>
       <h1>{title}</h1>
       <h2>{subTitle}</h2>
-    </div>
+    </TitleWrapper>
   );
 };
 
