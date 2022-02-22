8bac90844a8753c942da2b605fe05f816135f23e
Soumyadev Sanyal <soumya.sp35@gmail.com>
2022-02-22 11:25:10 +0530

    Starred page finished.

--- src/pages/Starred.js
+++ src/pages/Starred.js
@@ -1,9 +1,47 @@
 /* eslint-disable react/function-component-definition */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { useShows } from '../misc/custom-hooks';
+import { apiGet } from '../misc/config';
+import ShowGrid from '../components/shows/ShowGrid';
 
 const Starred = () => {
-  return <MainPageLayout>This is starred page</MainPageLayout>;
+  const [starred] = useShows();
+
+  // eslint-disable-next-line no-unused-vars
+  const [shows, setShows] = useState(null);
+  // eslint-disable-next-line no-unused-vars
+  const [isLoading, setIsLoading] = useState(true);
+  // eslint-disable-next-line no-unused-vars
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (starred && starred.length > 0) {
+      const promises = starred.map(showId => apiGet(`shows/${showId}`));
+
+      Promise.all(promises)
+        .then(apiData => apiData.map(show => ({ show })))
+        .then(results => {
+          setShows(results);
+          setIsLoading(false);
+        })
+        .catch(err => {
+          setError(err.message);
+          setIsLoading(false);
+        });
+    } else {
+      setIsLoading(false);
+    }
+  }, [starred]);
+
+  return (
+    <MainPageLayout>
+      {isLoading && <div>Shows are still loading.</div>}
+      {error && <div>Error occured:{error}</div>}
+      {!isLoading && !shows && <div>No shows were added.</div>}
+      {!isLoading && !error && shows && <ShowGrid data={shows} />}
+    </MainPageLayout>
+  );
 };
 
 export default Starred;
